01740a09d123aba430dea4b6d4a2ec4e6b657588
Leonhardt Koepsell <leonhardt@koepsell.io>
2017-10-09 09:29:44 -0700

    Add JUnit5 dependencies

--- data-nest/build.gradle.kts
+++ data-nest/build.gradle.kts
@@ -1,8 +1,22 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    val junitPlatformVersion = "1.0.1"
+    dependencies {
+        classpath("org.junit.platform:junit-platform-gradle-plugin:$junitPlatformVersion")
+    }
+}
+
 plugins {
     val kotlinVersion = "1.1.51"
     kotlin("jvm") version kotlinVersion
+}
+
+apply {
+    plugin("org.junit.platform.gradle.plugin")
 }
 
 group = "io.github.codebandits"
@@ -18,6 +32,11 @@ repositories {
     mavenCentral()
 }
 
+val junitJupiterVersion = "5.0.1"
+
 dependencies {
     compile("org.jetbrains.kotlin:kotlin-stdlib-jre8")
+
+    testCompile("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
+    testRuntime("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
 }
